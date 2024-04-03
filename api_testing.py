339f041ca72562eb3cbbf88ce39924de30bd0659
Kyle Zhan <20kzhan@gmail.com>
2024-04-02 21:38:44 -0700

    ACTUALLY fix all merge errors

--- api_testing.py
+++ api_testing.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import http.client
 
 conn = http.client.HTTPSConnection("dev-op4dpb8txjx5ikl5.us.auth0.com")
@@ -12,19 +11,4 @@ conn.request("POST", "/oauth/token", payload, headers)
 res = conn.getresponse()
 data = res.read()
 
-=======
-import http.client
-
-conn = http.client.HTTPSConnection("dev-op4dpb8txjx5ikl5.us.auth0.com")
-
-payload = "{\"client_id\":\"rpLJXdxsMFs6ZExGZeU7ia9TrJL3ZhNh\",\"client_secret\":\"FnCn5coFxkBd0STQdq7ZrF74rWWIkegTOwFDieo-OBzBAxCPaPCNdelIoEcItW7S\",\"audience\":\"https://dev-op4dpb8txjx5ikl5.us.auth0.com/api/v2/\",\"grant_type\":\"client_credentials\"}"
-
-headers = { 'content-type': "application/json" }
-
-conn.request("POST", "/oauth/token", payload, headers)
-
-res = conn.getresponse()
-data = res.read()
-
->>>>>>> f0664b25e0e2c0621c944bed919f34efc1cc8918
 print(data.decode("utf-8"))
--- extras.py
+++ extras.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import logging
 import pprint
 
@@ -9,35 +8,4 @@ class P(pprint.PrettyPrinter):
     if isinstance(object, str):
       if len(object) > 20:
         object = object[:20] + '...'
-=======
-import logging
-import pprint
-
-class CustomFormatter(logging.Formatter):
-    grey = "\x1b[38;20m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[31;20m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
-    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
-
-    FORMATS = {
-        logging.DEBUG: grey + format + reset,
-        logging.INFO: grey + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset
-    }
-
-    def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
-
-class P(pprint.PrettyPrinter):
-  def _format(self, object, *args, **kwargs):
-    if isinstance(object, str):
-      if len(object) > 20:
-        object = object[:20] + '...'
->>>>>>> f0664b25e0e2c0621c944bed919f34efc1cc8918
     return pprint.PrettyPrinter._format(self, object, *args, **kwargs)
