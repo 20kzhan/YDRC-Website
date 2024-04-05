38978df9d5b61d5deb51cc2fb3d3c2d4b28dd045
Kyle Zhan <20kzhan@gmail.com>
2024-04-04 22:30:51 -0700

    help

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ import time
 from urllib.parse import quote_plus, urlencode
 
 from os import environ as env
-
 from dotenv import load_dotenv
 
 from auth0.authentication import Database, GetToken
