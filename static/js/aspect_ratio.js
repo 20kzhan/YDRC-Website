e8705d100491ec86f4b634d2d857e8d60ee0538d
Kyle Zhan <20kzhan@gmail.com>
2024-09-18 00:00:01 -0700

    testing aspect ratio alerts

--- static/js/aspect_ratio.js
+++ static/js/aspect_ratio.js
@@ -1,7 +1,11 @@
 function gcd (a, b) {
     return (b == 0) ? a : gcd (b, a%b);
 }
-var w = screen.width;
-var h = screen.height;
+var w = window.innerWidth;
+var h = window.innerHeight;
 var r = gcd (w, h);
 console.log("Aspect Ratio = ", w/r, ":", h/r);
+
+if (w/r < h/r) {
+    alert("This website does not currently support devices with this aspect ratio. Please try landscape mode or a different device.");
+}
